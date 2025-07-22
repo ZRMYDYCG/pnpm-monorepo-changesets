c7db65ee34f9751b8bc85c9c71ad3d8ba2cfeba6
一勺 <116160870+ZRMYDYCG@users.noreply.github.com>
2025-07-22 05:15:47 +0000

    test: add logHandle to pkga.

--- packages/pkga/index.ts
+++ packages/pkga/index.ts
@@ -3,3 +3,7 @@ export const isEven = (x: number) => x % 2 === 0
 export const a = 0
 
 export const typeOf = (obj: any) => Object.prototype.toString.call(obj).slice(8, -1).toLowerCase()
+
+export function logPrint(text: string) {
+    console.log(text)
+}
